29e7617e9c4385ea3a948c641c4019131f91df89
harper.gao <871418277@qq.com>
2022-11-29 21:38:05 +0800

    Config resolve paths @FisherCore add vite support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
         ],
       },
     }),
+    tsconfigPaths(),
   ],
   test: {
     environment: 'jsdom',
