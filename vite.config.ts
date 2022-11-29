36743edf0c2d0c51ae0008bdfda7213b754537e2
harper.gao <871418277@qq.com>
2022-11-29 21:17:51 +0800

    Add vitest lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -13,4 +14,8 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 });
