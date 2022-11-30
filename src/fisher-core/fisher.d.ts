ed98171fc1f88fbddd50c5264634a9cc335b94f2
harper.gao <871418277@qq.com>
2022-11-30 17:31:03 +0800

    Update fisher-core types

---
+++ src/fisher-core/fisher.d.ts
@@ -0,0 +1,7 @@
+declare module fisher {
+  const activeActionId: string;
+
+  // type definetion for window
+  const fisherGold: import('./fisher-gold').FisherGold;
+  const fisherBackpack: import('./fisher-backpack').FisherBackpack;
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+/// <reference path="./fisher-core/fisher.d.ts" />
