22cd97cfd45e9f019068a99f013576c8d2875010
harper.gao <871418277@qq.com>
2022-12-24 14:51:02 +0800

    Build Fisher Battle module and demo

--- src/pages/demo/FisherBattleDemo.tsx
+++ src/pages/demo/FisherBattleDemo.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import { Button, Stack, Typography } from '@mui/material';
 import { DemoLayout } from './DemoLayout';
-import { FisherBattle } from '../../fisher-core/fisher-battle';
-import { FisherPerson } from '../../fisher-core/fisher-person';
 import { FisherPersonDemo } from './FisherPersonDemo';
-import { observer } from 'mobx-react';
+import { Enemy, FisherPerson } from '@FisherCore';
+import { FisherBattle } from '../../fisher-core/fisher-battle';
 
 export const FisherBattleDemo = observer(
   class FisherBattleDemo extends React.Component<any, any> {
@@ -12,25 +12,20 @@ export const FisherBattleDemo = observer(
       super(props);
       this.state = {
         battle: new FisherBattle(),
-        enemy: undefined,
       };
     }
 
     public initializeBattle = () => {
-      const enemy = new FisherPerson();
+      const enemy = new Enemy();
       enemy.initialize({
         name: '任我行',
-        mode: FisherPerson.Mode.Enemy,
         level: FisherPerson.Level.GasRefiningMiddle,
       });
-      this.setState({
-        enemy: enemy,
-      });
-      this.state.battle.initialize({ master: fisher.master, enemy });
+      this.state.battle.initialize({ enemy });
     };
 
     render() {
-      const { battle, enemy } = this.state;
+      const { battle } = this.state;
       return (
         <DemoLayout title="战斗模块">
           <Typography>{battle.inBattle ? '战斗中' : '等待战斗'}</Typography>
@@ -42,7 +37,7 @@ export const FisherBattleDemo = observer(
           )}
           <Stack direction="row">
             <FisherPersonDemo person={fisher.master} />
-            {enemy && <FisherPersonDemo person={enemy} />}
+            {battle.enemy && <FisherPersonDemo person={battle.enemy} />}
           </Stack>
         </DemoLayout>
       );
