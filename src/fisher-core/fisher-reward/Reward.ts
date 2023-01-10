58d65222c0089e3cff4de038266cf84dc11cb758
harper.gao <871418277@qq.com>
2023-01-10 16:28:43 +0800

    Refactor Reward interfaces

--- src/fisher-core/fisher-reward/Reward.ts
+++ src/fisher-core/fisher-reward/Reward.ts
@@ -5,26 +5,19 @@ import { Skill } from '../fisher-skill';
 import { bank } from '../fisher-bank';
 import { backpack } from '../fisher-backpack';
 import { store } from '../fisher-packages';
+import { core } from '../fisher-core';
 
 interface ICreateRewardOptions {
   gold?: number;
   itemId?: string;
   itemQuantity?: number;
-  skill?: ICreateSkillReward;
-}
-
-interface ICreateSkillReward {
-  skill: Skill;
-  experience: number;
+  componentId?: string;
+  experience?: number;
 }
 
 export class Reward {
   static readonly logger = prefixLogger(prefixes.FISHER_CORE, 'Reward');
-  /**
-   * 物品奖励
-   * @type {Map<Item, number>}
-   * @memberof Reward
-   */
+
   public rewardItemMap: Map<Item, number> = new Map();
 
   public get rewardItems() {
@@ -35,24 +28,15 @@ export class Reward {
     return this.rewardItemMap.size > 0;
   }
 
-  /**
-   * 金币奖励
-   */
   public rewardGold: number = 0;
 
   public get hasRewardGold() {
     return this.rewardGold > 0;
   }
 
-  /**
-   * 技能经验奖励
-   */
   public rewardSkillExperience: Map<Skill, number> = new Map();
 
-  /**
-   * 创建奖励
-   */
-  static create = ({ gold, itemId, itemQuantity, skill }: ICreateRewardOptions): Reward => {
+  static create = ({ gold, itemId, itemQuantity, componentId, experience }: ICreateRewardOptions): Reward => {
     const reward = new Reward();
 
     const _gold = gold ?? 0;
@@ -65,17 +49,18 @@ export class Reward {
       reward.addRewardItem(item, itemQuantity ?? 1);
     }
 
-    if (skill !== undefined) {
-      reward.addRewardSkill(skill.skill, skill.experience);
+    if (componentId !== undefined && experience !== undefined) {
+      const component = core.findSkillComponentById(componentId);
+      reward.addRewardSkill(component.skill, experience);
     }
 
     return reward;
   };
 
   /**
-   * 创建随机奖励
-   * 如果没有命中概率则返回 undefined
-   * 如果命中则创建奖励
+   * create random reward
+   * if didn't hit the probability return undefined
+   * if hit probability create rewards and return
    */
   static createRandomReward = (probability: number, options: ICreateRewardOptions): Reward | undefined => {
     if (!roll(probability)) {
@@ -85,11 +70,6 @@ export class Reward {
     return Reward.create(options);
   };
 
-  /**
-   * 添加物品奖励
-   * 如果要添加的奖励物品已经存在，奖励物品数量 + quantity
-   * 如果不存在要添加的奖励物品，则插入
-   */
   public addRewardItem = (item: Item, quantity: number): this => {
     const hasRewardItem = this.rewardItemMap.has(item);
 
@@ -107,9 +87,6 @@ export class Reward {
     return this;
   };
 
-  /**
-   * 添加金钱奖励
-   */
   public addRewardGold = (gold: number): this => {
     this.rewardGold += gold;
     return this;
@@ -120,11 +97,6 @@ export class Reward {
     return this;
   };
 
-  /**
-   * 添加技能经验奖励
-   * 如果要添加的技能已经存在
-   * 奖励技能经验 + experience
-   */
   public addRewardSkill = (skill: Skill, experience: number): this => {
     const hasRewardSkill = this.rewardSkillExperience.has(skill);
 
@@ -143,32 +115,49 @@ export class Reward {
     return this;
   };
 
-  /**
-   * 清空所有奖励
-   */
-  public resetRewards = () => {
+  public reset = () => {
     this.rewardGold = 0;
     this.rewardItemMap.clear();
     this.rewardSkillExperience.clear();
     Reward.logger.debug('Success reset rewards.');
   };
 
-  /**
-   * 执行奖励
-   */
-  public executeRewards = () => {
+  public execute = () => {
+    this.executeGold();
+    this.executeRewardItems();
+    this.executeSkillExperience();
+  };
+
+  private executeGold = () => {
     if (this.rewardGold > 0) {
       bank.receiveGold(this.rewardGold);
       Reward.logger.debug('Execute reward gold: ' + this.rewardGold);
     }
+  };
 
+  private executeRewardItems = () => {
     if (this.rewardItemMap.size > 0) {
       this.rewardItemMap.forEach((quantity, rewardItem) => {
-        backpack.addItem(rewardItem, quantity);
-        Reward.logger.debug('Execute reward item: ' + rewardItem.name, 'quantity: ' + quantity);
+        if (quantity > 0) {
+          this.executeAddItem(rewardItem, quantity);
+        } else {
+          this.executeReduceItem(rewardItem, quantity);
+        }
       });
     }
+  };
 
+  private executeAddItem = (rewardItem: Item, quantity: number) => {
+    backpack.addItem(rewardItem, quantity);
+    Reward.logger.debug('Execute add item: ' + rewardItem.name, 'quantity: ' + quantity);
+  };
+
+  private executeReduceItem = (rewardItem: Item, quantity: number) => {
+    backpack.reduceItem(rewardItem, quantity);
+    Reward.logger.debug('Execute reduce item: ' + rewardItem.name, 'quantity: ' + quantity);
+  };
+
+  private executeSkillExperience = () => {
     if (this.rewardSkillExperience.size > 0) {
       this.rewardSkillExperience.forEach((experience, rewardSkill) => {
         rewardSkill.addExperience(experience);
