71dcce279f5059d28a283a634ac46783ecbfa455
harper.gao <871418277@qq.com>
2023-01-16 16:39:06 +0800

    Add avatar assets

--- src/fisher-core/assets/Assets.ts
+++ src/fisher-core/assets/Assets.ts
@@ -1,8 +1,11 @@
 import coin from './common/coin.png';
-
 import woodsword from './equipments/woodsword.png';
+import avatar from './person/avatar.png';
 
 export const Assets = {
   coin,
+  // equipment assets
   woodsword,
+  // person assets
+  avatar,
 };
