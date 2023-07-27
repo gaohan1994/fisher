783d52bd9efd6ae20455203f3c36620282ed7e23
gaohan1994 <871418277@qq.com>
2023-07-28 00:02:13 +0800

    Build @fisher/ioc

---
+++ packages/ioc/src/__test__/ioc.test.ts
@@ -0,0 +1,35 @@
+import { service, IocService, inject } from '../ioc';
+
+@service('A')
+class ServiceA {
+  id = 'A';
+}
+
+@service('B')
+class ServiceB {
+  id = 'B';
+  constructor(@inject('A') public readonly serviceA: ServiceA) {}
+}
+
+@service('C')
+class ServiceC {
+  id = 'C';
+  constructor(@inject('A') readonly serviceA: ServiceA, @inject('B') readonly serviceB: ServiceB) {}
+}
+
+it('should success register service', () => {
+  const iocService = new IocService();
+  iocService.init();
+
+  // Node iocService will also register iocService itself!!
+  expect(iocService.serviceStore.size).toEqual(4);
+
+  const serviceA: ServiceA = iocService.getService('A');
+  const serviceB: ServiceB = iocService.getService('B');
+  const serviceC: ServiceC = iocService.getService('C');
+
+  expect(serviceB.serviceA).toEqual(serviceA);
+
+  expect(serviceC.serviceA).toEqual(serviceA);
+  expect(serviceC.serviceB).toEqual(serviceB);
+});
---
+++ packages/ioc/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Need IOC to handle services
+ * Register all moduels dependencies
+ */
+
+export * from './graph';
+
+export * from './ioc';
